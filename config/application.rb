cbd79d9af9b694f48d037f57fb09ce82d5ca9959
Alexandr Zimin <aleksander.zimin@gmail.com>
2023-02-01 12:57:30 +0300

    add timezone Moscow

--- config/application.rb
+++ config/application.rb
@@ -30,6 +30,7 @@ module TestGame
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.time_zone = "Moscow"
 
     # Don't generate system test files.
     config.generators.system_tests = nil
