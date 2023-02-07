af29b3859aca0e2ad8162be3a96cb5830bd3fb05
Alexandr Zimin <aleksander.zimin@gmail.com>
2023-02-07 08:09:37 +0300

    add relationship rspec

--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Relationship, type: :model do
-  it { should allow_value(true).for(:mark) }
+  it { should belong_to :player }
+  it { should belong_to :player_indicator }
+  it { should belong_to :team }
+  it { should belong_to :match }
 end
